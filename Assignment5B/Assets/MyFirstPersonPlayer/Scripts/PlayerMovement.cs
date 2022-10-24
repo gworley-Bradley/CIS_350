c2b2bba01b41557d7f91aea081e0cd454ca669f2
gworley-Bradley <81546469+gworley-Bradley@users.noreply.github.com>
2022-10-23 23:53:41 -0500

    Assignment 5B

---
+++ Assignment5B/Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Vector3 velocity;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 2f;
+    public float jumpHeight = 3f;
+
+    //variables for checking if on the ground
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+    private void Awake()
+    {
+        gravity *= gravityMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //check if the player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+        //add gravity to velocity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+}
